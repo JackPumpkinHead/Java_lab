9c59c345c5a39f2dfa6c978f6b79e002f145eb6c
JackPumpkinHead <kostyu444enko.igor@gmail.com>
2022-11-16 14:51:44 +0300

    feature/without_doc

---
+++ 1.2/src/mypackage/HelloWorld.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+import mypackage.Mood;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+	
+		Mood mood = new Mood();
+		mood.Happily("I`m");
+		mood.Sadly("You are");
+	}
+
+}
+
---
+++ 1.2/src/mypackage/Mood.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class Mood {
+
+	public static void Happily(String face) {
+		System.out.println(face +"))");
+	}
+	
+	public void Sadly(String face) {
+		System.out.println(face+ "((");
+	} 
+
+}
